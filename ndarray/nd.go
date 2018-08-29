be2e3e3212291cf4719a07ea4a5f4303a0362b54
Sebastien Binet <binet@cern.ch>
2018-08-29 22:11:58 +0200

    ndarray: add contract

--- ndarray/nd.go
+++ ndarray/nd.go
@@ -1,18 +1,25 @@
 package ndarray
 
-type Array(type T) struct {
+contract Elem(x T) {
+	x += x
+	x = x
+	x == x
+	x -= x
+}
+
+type Array(type T Elem) struct {
 	data  []T
 	shape []int
 }
 
-func New(type T)(shape []int, data []T) Array {
-	return Array{
+func New(type T Elem)(shape []int, data []T) Array(T) {
+	return Array(T){
 		data:  data,
 		shape: shape,
 	}
 }
 
-func Sum(type T)(arr Array, axis []int) T {
+func Sum(type T Elem)(arr Array(T), axis []int) T {
 	var sum T
 	for _, v := range arr.data {
 		sum += v
